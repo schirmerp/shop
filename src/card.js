256a1fafa1d6324917d9652fe00a3bc81a1298a9
Joe <schirmer.pc@gmail.com>
2021-12-09 15:12:02 -0500

    works now

--- src/card.js
+++ src/card.js
@@ -9,7 +9,7 @@ export default function Card (props){
             $<span className="item-price">{props.price}</span>
             <p className="item-description">{props.description}</p>
             <form className="form" onSubmit={props.onSubmit}>
-            <input name="amount" id="item-amount" type="number" placeholder="enter amount" />
+            <input name="amount" id="item-amount" type="number" min="0" placeholder="enter amount" />
             <button onClick={props.onClick}>Add to Cart</button>
             </form>
         </div>
