f74be9b43def9643d660d8f81a849dfb95e87937
Joe <schirmer.pc@gmail.com>
2021-12-08 23:30:27 -0500

    sadads

--- src/RouterSwitch.js
+++ src/RouterSwitch.js
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
 import Shop from "./shop";
 
 const RouterSwitch = () =>{
     return(
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/" element={<App />} />
                 <Route path="/shop" element={<Shop />}></Route>
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     )
 }
 
